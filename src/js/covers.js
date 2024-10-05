4166b33d7f940b59c265571d0a61ce97235a0594
dvankevich <dvankevich@gmail.com>
2024-10-05 12:44:25 +0300

    add covers script

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,1 +1,12 @@
-console.log("covers");
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      console.log('Елемент у видимій зоні!');
+    } else {
+      console.log('Елемент не у видимій зоні.');
+    }
+  });
+});
+
+const targetElement = document.querySelector('.covers');
+observer.observe(targetElement);
