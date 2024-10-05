797880ea27519441c0e6f7ff23e7eb828dd625e1
dvankevich <dvankevich@gmail.com>
2024-10-05 18:02:12 +0300

    covers disable animations script. fix hero background

--- src/js/covers.js
+++ src/js/covers.js
@@ -2,11 +2,27 @@ const observer = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       console.log('Елемент у видимій зоні!');
+      animationOn(cards);
     } else {
       console.log('Елемент не у видимій зоні.');
+      animationOff(cards);
     }
   });
 });
 
+function animationOff(items) {
+  items.forEach(element => {
+    element.style.animationPlayState = 'paused';
+  });
+}
+
+function animationOn(items) {
+  items.forEach(element => {
+    element.style.animationPlayState = 'running';
+  });
+}
+
 const targetElement = document.querySelector('.covers');
 observer.observe(targetElement);
+
+const cards = document.querySelectorAll('.marquee__line');
