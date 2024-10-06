38dffcc8d874f2bbbe220670b2871c1dbc128a28
dvankevich <dvankevich@gmail.com>
2024-10-06 20:41:38 +0300

    add credo and photo. add accordeon

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,1 +1,6 @@
-console.log("about-me");
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+new Accordion('.about-me-accordion-container');
